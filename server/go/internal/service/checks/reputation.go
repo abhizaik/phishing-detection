f57192d622603b3c36f12c9eb7ae44e174378f70
abhizaik <65501471+abhizaik@users.noreply.github.com>
2025-08-04 13:35:58 +0530

    feat: add reputation check

---
+++ server/go/internal/service/checks/reputation.go
@@ -0,0 +1,22 @@
+package checks
+
+import "github.com/abhizaik/SafeSurf/internal/constants"
+
+func IsTrustedTld(domain string) (bool, bool) {
+	tld, icann := GetTld(domain)
+	_, ok := constants.TrustedTLDs[tld]
+
+	return ok, icann
+}
+
+func IsRiskyTld(domain string) (bool, bool) {
+	tld, icann := GetTld(domain)
+	_, ok := constants.RiskyTLDs[tld]
+
+	return ok, icann
+}
+
+func IsUrlShortener(domain string) bool {
+	_, ok := constants.URLShorteners[domain]
+	return ok
+}
